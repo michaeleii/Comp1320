a2b0f09db6a0bc16997c2088df08e07ee1ad0db4
michaellei-e0ca <michaelleie0ca@gmail.com>
2022-09-26 13:26:18 -0700

    Created userInput variable containing user inputs from the terminal

--- Lab3/main.js
+++ Lab3/main.js
@@ -1,1 +1,5 @@
 const mathHelper = require("./mathHelper");
+
+process.argv.splice(0, 2); //Removes the first 2 elements from process.argv array so we can have just the user inputs
+
+const userInput = process.argv;
