f98d22118632c016ee3396a675bbc7cde7cd59dd
Michael Lei <michaelleie0ca@gmail.com>
2022-09-26 22:23:25 -0700

    Added distance formula to processInput function

--- Lab3/main.js
+++ Lab3/main.js
@@ -8,6 +8,8 @@ const userInput = process.argv; //Contains array of what the user inputted.
 
 let folder = "dataPoints"; //Name of the folder we will create
 
+const distance = mathHelper.distance; //Makes imported distance formula shorter and easier to write
+
 const processInput = (input, path) => {
 	//Creates a folder
 	fs.mkdir(path, (err) => {
@@ -15,11 +17,19 @@ const processInput = (input, path) => {
 			path = "temp_" + path; //Adds a temp_ to the path name if path name is already created.
 			processInput(input, path); //Recursively call the processInput function and make a folder with new path name.
 		}
-		const data = "";
-		fs.writeFile(path, data, (err) => {
+
+		const result = distance(
+			userInput[0],
+			userInput[1],
+			userInput[2],
+			userInput[3]
+		); //Calculates the distance of points. (Imported from mathHelper.js)
+
+		fs.writeFile(path, result, (err) => {
 			if (err) {
 				console.log(err);
 			}
+			console.log(`Your contents have been saved to: ${path}`);
 		});
 	});
 };
