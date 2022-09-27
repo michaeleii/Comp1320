6939e562b62b01e57f1bbf8d5ddf26991bb8240a
Michael Lei <michaelleie0ca@gmail.com>
2022-09-26 22:12:33 -0700

    Added writeFile function to processInput function

--- Lab3/main.js
+++ Lab3/main.js
@@ -4,7 +4,8 @@ const fs = require("fs");
 
 process.argv.splice(0, 2); //Removes the first 2 elements from process.argv array so we can have just the user inputs.
 
-const userInput = process.argv; //
+const userInput = process.argv; //Contains array of what the user inputted.
+
 let folder = "dataPoints"; //Name of the folder we will create
 
 const processInput = (input, path) => {
@@ -14,6 +15,12 @@ const processInput = (input, path) => {
 			path = "temp_" + path; //Adds a temp_ to the path name if path name is already created.
 			processInput(input, path); //Recursively call the processInput function and make a folder with new path name.
 		}
+		const data = "";
+		fs.writeFile(path, data, (err) => {
+			if (err) {
+				console.log(err);
+			}
+		});
 	});
 };
 
