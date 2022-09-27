142419a49090e5d22042ae97c5c2d5939f397669
michaellei-e0ca <michaelleie0ca@gmail.com>
2022-09-26 19:33:28 -0700

    Added processInput function

--- Lab3/main.js
+++ Lab3/main.js
@@ -3,3 +3,13 @@ const mathHelper = require("./mathHelper");
 process.argv.splice(0, 2); //Removes the first 2 elements from process.argv array so we can have just the user inputs
 
 const userInput = process.argv;
+
+const processInput = (input) => {
+	fs.mkdir("/dataPoints");
+	fs.writeFile("", content, (err) => {
+		if (err) {
+			console.error(err);
+		}
+		// file written successfully
+	});
+};
