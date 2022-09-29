d95194f06ce2a35414b4f54cb8d0c4a1cb7f2fcf
Michael Lei <michaelleie0ca@gmail.com>
2022-09-28 20:08:47 -0700

    Reverted rounding distance

--- Lab3/mathHelper.js
+++ Lab3/mathHelper.js
@@ -4,7 +4,7 @@ const square = (num) => Math.pow(num, 2); // Returns the square of a number.
 
 const distance = (x1, y1, x2, y2) => {
 	let insideSquareRoot = square(x2 - x1) + square(y2 - y1);
-	return Math.round(squareRoot(insideSquareRoot)); //Returns distance to the nearest whole number.
+	return squareRoot(insideSquareRoot).toFixed(4); //Returns distance to 6 decimal places.
 };
 
 module.exports = { distance };
